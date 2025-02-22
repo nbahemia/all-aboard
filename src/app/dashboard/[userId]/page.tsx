311c71ff846add97071479e7c88f0d95fdaf19f8
Naeem Bahemia <naeembahemia@gmail.com>
2025-02-22 13:48:33 -0500

    Created frame work for the Dashboard page, made for each user. This includes a Sidebar component containing a search bar and Club drop down check box list for when club data gets taken from our database. All these components came from Flowbite css.

---
+++ src/app/dashboard/[userId]/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+
+function Dashboard() {
+
+  return (
+    <div className='bg-[#2D336B]'>
+        
+    </div>
+  )
+}
+
+export default Dashboard
